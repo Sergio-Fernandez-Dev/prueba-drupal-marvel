1bfe7e6f401de1b11a973e59e201145f31e02b40
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2023-04-04 09:31:05 +0200

    create interfaces for custom entities

---
+++ web/modules/custom/marvel/src/CharacterInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\marvel;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Provides an interface defining a character entity type
+ */
+interface CharacterInterface extends ContentEntityInterface {
+
+}
---
+++ web/modules/custom/marvel/src/ComicInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\marvel;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Provides an interface defining a comic entity type
+ */
+interface ComicInterface extends ContentEntityInterface {
+
+}
