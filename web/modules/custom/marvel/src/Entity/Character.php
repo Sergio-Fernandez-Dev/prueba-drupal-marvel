8f7f49c452d06e7ec0892f2e50b499112a1bd546
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2023-04-04 09:32:17 +0200

    create custom entities skeletons

---
+++ web/modules/custom/marvel/src/Entity/Character.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Drupal\marvel\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use \Drupal\Core\Entity\Annotation\ContentEntityType;
+
+/**
+ * Defines a character entity type.
+ * 
+ * @ContentEntityType
+ */
+public class Character extends ContentEntityBase implements CharacterInterface {
+
+
+} 
---
+++ web/modules/custom/marvel/src/Entity/Comic.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Drupal\marvel\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use \Drupal\Core\Entity\Annotation\ContentEntityType;
+
+/**
+ * Defines a comic entity type.
+ * 
+ * @ContentEntityType
+ */
+public class Comic extends ContentEntityBase implements ComicInterface {
+
+
+} 
