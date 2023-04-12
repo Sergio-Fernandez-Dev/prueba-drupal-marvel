f67824954d18ede7b60ce0d1f7b706ce0603afea
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2023-04-12 09:59:25 +0200

    save character id in the database

--- web/modules/custom/marvel/src/Controller/CharacterController.php
+++ web/modules/custom/marvel/src/Controller/CharacterController.php
@@ -1,12 +1,36 @@
 <?php
 
 namespace Drupal\marvel\Controller;
+
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\marvel\Entity\Character;
+use Symfony\Component\HttpFoundation\Response;
 
 class CharacterController extends ControllerBase
 {
     public function index()
     {
         echo 'Character Controller';
+    }
+
+    /**
+   * @param Request $request
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   */
+    public function store(Request $request)
+    {               
+        $data = json_decode($request->getContent(), true);
+        $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+
+        $character = Character::create([
+            'character_id' => $data['id'],          
+            'users' => $user,
+        ]);
+
+        $character->save();
+
+        return new JsonResponse(Response::HTTP_OK);
     }
 }
